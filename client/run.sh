3dced0519e19391b92716fdd89632fd97718a0f8
haaz77 <haaz77@gmail.com>
2018-07-27 17:25:35 +0300

    Update run.sh

--- client/run.sh
+++ client/run.sh
@@ -13,7 +13,18 @@ cat  > /etc/security/limits.conf <<EOF
 * hard memlock unlimited
 * soft memlock unlimited
 EOF
-
+cd nginx-/client/
+cp nginx.conf /usr/local/nginx/conf/nginx.conf
+cp index.php /var/www/
+cp www.conf /etc/php-fpm.d/www.conf 
+cp nginxd /etc/init.d/
+ chmod +x /etc/init.d/nginxd 
+ systemctl enable php-fpm
+ systemctl start  php-fpm
+ /etc/init.d/nginxd start
+ chkconfig php-fpm on
+ chkconfig nginxd on
+ 
 echo "Starting server..." 
 php-fpm &
 
