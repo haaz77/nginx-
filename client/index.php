61e978bcd1257d8401ff3f0e170732e6993830a7
haaz77 <haaz77@gmail.com>
2018-07-27 16:50:49 +0300

    Update index.php

--- client/index.php
+++ client/index.php
@@ -1,5 +1,5 @@
-    <?php
-    $values = array('http://secure-01.livehd7.top/hls/stream.m3u8','http://secure-02.livehd7.top/hls/stream.m3u8','http://secure-03.livehd7.top/hls/stream.m3u8','http://secure-04.livehd7.top/hls/stream.m3u8','http://secure-05.livehd7.top/hls/stream.m3u8','http://secure-06.livehd7.top/hls/stream.m3u8','http://secure-07.livehd7.top/hls/stream.m3u8','http://secure-08.livehd7.top/hls/stream.m3u8','http://secure-09.livehd7.top/hls/stream.m3u8');
+<?php
+    $values = array('http://c3.str.dak32.com/hls/stream.m3u8','http://c2.str.dak32.com/hls/stream.m3u8');
     //echo $values[array_rand($values)];
     ?>
 <head>
@@ -10,7 +10,6 @@ div#main {
     width: 100%;
     height: 100%;
 }
-
 div#Blog1 {
     height: 100%;
     width: 100%;
@@ -19,7 +18,6 @@ html, body {
   height: 100%;
   width: 100%;
 }
-
 body {
   margin: 0;
   padding: 0;
